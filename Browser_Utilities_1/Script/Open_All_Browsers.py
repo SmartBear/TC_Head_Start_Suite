4892099878b2bed74bb0c32dade42595825b7fe9
Kevin-M-Johnson <101289324+Kevin-M-Johnson@users.noreply.github.com>
2022-06-24 19:57:00 -0400

    Initial Commit

---
+++ Browser_Utilities_1/Script/Open_All_Browsers.py
@@ -0,0 +1,17 @@
+﻿def Open_All_Browsers():
+    #Open All Installed Browsers and leave them open
+    #Runs a keyword test.
+    KeywordTests.Get_TestExecute_Version_Information.Run()
+    #Iterates through the specified browsers.
+    i = 0
+    while i < Browsers.Count:
+        Browsers.Item[i].Run()
+        #Delays the test execution for the specified time period.
+        Delay(1000, "Generic 1 Second Delay")
+        #Maximizes the specified Window object.
+        Aliases.browser.BrowserWindow.Maximize()
+        #Delays the test execution for the specified time period.
+        Delay(1000, "Generic 1 Second Delay")
+        #Posts an information message to the test log.
+        Log.Message("Browser Name = " + Aliases.browser.Name + " :Version = " + aqConvert.VarToStr(Aliases.browser.FileVersionInfo) + " :Process Type = " + Aliases.browser.ProcessType, "", pmNormal, Project.Variables.LogAtrribInformation)
+        i = i + 1
---
+++ Orders_Desktop_App_Project_1/Script/Unit1.js
@@ -0,0 +1,19 @@
+﻿function Test1()
+{
+  //Posts an information message to the test log.
+  Log.Message("Hello world (log message)", "");
+}
+
+function Test2()
+{
+  //Runs the "Orders" tested application.
+  TestedApps.Orders.Run();
+  //Moves the mouse cursor to the menu item specified and then simulates a single click.
+  Aliases.Orders.MainForm.MainMenu.Click("View|List");
+  //Moves the mouse cursor to the menu item specified and then simulates a single click.
+  Aliases.Orders.MainForm.MainMenu.Click("View|Small Icons");
+  //Moves the mouse cursor to the menu item specified and then simulates a single click.
+  Aliases.Orders.MainForm.MainMenu.Click("View|Large Icons");
+  //Closes the 'MainForm' window.
+  Aliases.Orders.MainForm.Close();
+}
