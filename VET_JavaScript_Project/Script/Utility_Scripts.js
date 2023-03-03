89ab828f10bca85fa3665f727327f645bd1f65e1
Kevin-M-Johnson <101289324+Kevin-M-Johnson@users.noreply.github.com>
2023-03-02 22:51:50 -0500

    Updates to add Tests for String Manipulation

--- VET_JavaScript_Project/Script/Utility_Scripts.js
+++ VET_JavaScript_Project/Script/Utility_Scripts.js
@@ -9,3 +9,39 @@
   else
     Log.Message("There are no occurrences of '" + aSubString + "' in '" + aString + "'")
 }
+
+function RemovingSubString()
+{
+  var Str = "123456789";
+  var StartPos = 3;
+  var sLength = 4;
+  var Res = aqString.Remove(Str, StartPos, sLength);
+  
+  Log.Message(Res); // Posts "12389"
+}
+  
+function Extract_Substring()
+{
+  var Str = "LeadingString0123456789TrailingString";
+  Log.Message(Str);
+  var StartPos = 0;
+  var sLength = 13;
+  var Res = aqString.Remove(Str, StartPos, sLength);
+  Log.Message(Res);
+  var StartPos = 10;
+  var sLength = 14;
+  var Res = aqString.Remove(Res, StartPos, sLength);
+  Log.Message(Res);
+}
+  
+function Extract_Substring_2(Phrase, Pos1, Length1, Pos2, Length2)
+{
+  var Str = Phrase;
+  Log.Message(Str);
+  var Str = aqString.Remove(Phrase, Pos1, Length1);
+  Log.Message(Str);
+  var Str = aqString.Remove(Str, Pos2, Length2);
+  Log.Message(Str);
+  Project.Variables.RefNumber = aqConvert.StrToInt(Str);
+  Log.Message(Project.Variables.RefNumber);
+}
