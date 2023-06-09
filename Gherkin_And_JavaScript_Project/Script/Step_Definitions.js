021937ad390c43cc74761a488fa93c10e7d77c4d
dermot.canniffe <dermot.canniffe@smartbear.com>
2023-06-09 16:04:22 +0100

    Fix Step Defs

--- Gherkin_And_JavaScript_Project/Script/Step_Definitions.js
+++ Gherkin_And_JavaScript_Project/Script/Step_Definitions.js
@@ -1,40 +1,40 @@
 ﻿// 0 - Common
 
 Given("Chrome is available", function (){
-   KeywordTests.Mod_LaunchBrowser("Chrome");
+   KeywordTests.Mod_LaunchBrowser.Run("Chrome");
 });
 
 When("we load the application url", function (){
-    KeywordTests.Mod_LoadWebStore();
+    KeywordTests.Mod_LoadWebStore.Run();
 });
 
 Then("the webStore loads", function (){
-    KeywordTests.Mod_WebStoreLoads();
+    KeywordTests.Mod_WebStoreLoads.Run();
 });
 
 Given("Firefox is available", function (){
-   KeywordTests.Mod_LaunchBrowser("Firefox");
+   KeywordTests.Mod_LaunchBrowser.Run("Firefox");
 });
 
 Given("Edge is available", function (){
-   KeywordTests.Mod_LaunchBrowser("Edge");
+   KeywordTests.Mod_LaunchBrowser.Run("Edge");
 });
 
 
 // 1 - Sanity
 
 Given("we can load our application", function (){
-    KeywordTests.Mod_LaunchBrowser("Firefox");
-    KeywordTests.Mod_LoadWebStore();
-    KeywordTests.Mod_WebStoreLoads();
+    KeywordTests.Mod_LaunchBrowser.Run("Firefox");
+    KeywordTests.Mod_LoadWebStore.Run();
+    KeywordTests.Mod_WebStoreLoads.Run();
 });
 
 When("we enter valid credentials", function (){
-    KeywordTests.Mod_EnterCreds("dermotc",Project.Variables.DermotPassword);
+    KeywordTests.Mod_EnterCreds.Run("dermotc",Project.Variables.DermotPassword);
 });
 
 Then("login is successful", function (){
-    KeywordTests.Mod_SuccessfulLogin("dermotc");
+    KeywordTests.Mod_SuccessfulLogin.Run("dermotc");
 });
 
 When("we enter Dermot and password", function (){
