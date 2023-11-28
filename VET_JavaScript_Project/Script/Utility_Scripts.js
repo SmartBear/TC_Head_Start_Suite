e9ca4c8d18587648b804d78eb9a50e88e364c7c9
Kevin-M-Johnson <101289324+Kevin-M-Johnson@users.noreply.github.com>
2023-11-28 15:25:29 -0500

    Added HeadLess_Browser.tcKDTest sample

--- VET_JavaScript_Project/Script/Utility_Scripts.js
+++ VET_JavaScript_Project/Script/Utility_Scripts.js
@@ -1,1 +1,11 @@
-﻿
+﻿function Test_Chrome_Headless()
+{
+  var server = "localhost";
+  var capabilities = {
+    "browserName": "chrome",
+    "screenResolution": "1920x1080"
+  };
+
+  var url = "https://bearstore-testsite.smartbear.com/";
+  Browsers.RemoteItem(server, capabilities).Run(url);
+ }
