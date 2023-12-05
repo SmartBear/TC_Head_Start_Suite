0cc90dfd12ba96107c9f805ccdf4c1f56966916f
Kevin-M-Johnson <101289324+Kevin-M-Johnson@users.noreply.github.com>
2023-12-05 11:03:00 -0500

    Added New Tests and modified the execution plan

--- VET_JavaScript_Project/Script/Utility_Scripts.js
+++ VET_JavaScript_Project/Script/Utility_Scripts.js
@@ -9,3 +9,17 @@
   var url = "https://bearstore-testsite.smartbear.com/";
   Browsers.RemoteItem(server, capabilities).Run(url);
  }
+ 
+function Test_Edge_Headless()
+{
+  var server = "localhost";
+  var capabilities = {
+    "browserName": "edge",
+    "screenResolution": "1920x1080"
+  };
+
+  var url = "https://bearstore-testsite.smartbear.com/";
+  Browsers.RemoteItem(server, capabilities).Run(url);
+ }
+  
+ 
