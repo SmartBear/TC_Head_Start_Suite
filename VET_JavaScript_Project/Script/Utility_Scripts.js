8490edd14aa59cf40c27043403692287b5480d1a
Kevin-M-Johnson <101289324+Kevin-M-Johnson@users.noreply.github.com>
2024-02-29 14:16:26 -0500

    Latest Updates with Local Headless Browser tests

--- VET_JavaScript_Project/Script/Utility_Scripts.js
+++ VET_JavaScript_Project/Script/Utility_Scripts.js
@@ -1,106 +1,100 @@
-﻿function Test_Chrome_Headless()
+﻿//Sample (not headless)
+function BearStoreTest_NotHeadless()
 {
-  var server = "localhost";
-  var capabilities = {
-    "browserName": "chrome",
-    "screenResolution": "1920x1080"
-  };
-
-  var url = "https://bearstore-testsite.smartbear.com/";
-  Browsers.RemoteItem(server, capabilities).Run(url);
- }
- 
-function Test_Edge_Headless()
-{
-  var server = "localhost";
-  var capabilities = {
-    "browserName": "edge",
-    "screenResolution": "1920x1080"
-  };
-
-  var url = "https://bearstore-testsite.smartbear.com/";
-  Browsers.RemoteItem(server, capabilities).Run(url);
- 
-  var browser = Sys.Browser();
-  var page = browser.page("*bearstore-testsite.smartbear.com*");
-  var searchBar = page.FindElement("//*[@id='header']/div[2]/div/div/div[1]/div[2]/form/input");
-  searchBar.click();
-  searchBar.value = "Ball";
-  page.FindElement("//*[@id='header']/div[2]/div/div/div[1]/div[2]/form/button").click();
-  Log.Picture(page.Picture(), "","");
-  page.CheckProperty(Aliases.browser.pageShop.sectionContent.panelPhTitle6, "contentText", cmpEqual, "Welcome to our store.");
- }
-  
-//Sample (not headless)
-function BearStoreTest()
-{
-  var webSite = "https://bearstore-testsite.smartbear.com"
-  Browsers.Item(btChrome).Run(webSite);
-  var browser = Sys.Browser();
-  var page = browser.page("*bearstore-testsite.smartbear.com*");
-  var searchBar = page.FindElement("//*[@id='header']/div[2]/div/div/div[1]/div[2]/form/input");
-  searchBar.click();
-  searchBar.value = "Ball";
-  page.FindElement("//*[@id='header']/div[2]/div/div/div[1]/div[2]/form/button").click();
-  page.Wait(5000);
-  Log.Picture(page.Picture(), "","");
+  Browsers.Item(btEdge).Run("https://bearstore-testsite.smartbear.com/");
+  let browser = Aliases.browser;
+  let browserWindow = browser.BrowserWindow;
+  browser.BrowserWindow.Maximize();
+  browser.pageShop.header.link.imageSmartstore.Click();
+  aqObject.CheckProperty(Aliases.browser.pageShop.FindElement("//span[.='Log in']"), "contentText", cmpEqual, "Log in");
+  aqObject.CheckProperty(Aliases.browser.pageShop.header.link.imageSmartstore, "title", cmpEqual, "SmartStore");
+  browserWindow.Close();
 }
 
-//Sample (headless)
-function HeadlessBearStoreTest()
+function Simple_Test_Script()
 {
-  var server = "localhost";
-  var capabilities = {
-    "browserName": "edge",
-    "screenResolution": "1920x1080"
-  };
-
-  var url = "https://bearstore-testsite.smartbear.com/";
-  Browsers.RemoteItem(server, capabilities).Run(url);
-
-  var browser = Sys.Browser();
-  var page = browser.page("*bearstore-testsite.smartbear.com*");
-  var searchBar = page.FindElement("//*[@id='header']/div[2]/div/div/div[1]/div[2]/form/input");
-  searchBar.click();
-  searchBar.value = "Ball";
-  page.FindElement("//*[@id='header']/div[2]/div/div/div[1]/div[2]/form/button").click();
-  page.Wait(5000);
-  
-  aqObject.CheckProperty(page.FindElement("//h1[contains(text(), 'Search result for')]"), "contentText", cmpEqual, "Search result for\nBall");
-  Log.Picture(page.Picture(), "Picture of Page","Detailed description");
-  
-}
-
-//Sample (headless)
-function HeadlessBearStoreTestII()
-{
-  var server = "localhost";
-  var capabilities = {
-    "browserName": "edge",
-    "screenResolution": "1920x1080"
-  };
-
-  var url = "https://bearstore-testsite.smartbear.com/";
-  Browsers.RemoteItem(server, capabilities).Run(url);
-
-  var browser = Sys.Browser();
-  var page = browser.page("*bearstore-testsite.smartbear.com*");
-  Log.Picture(page.Picture(), "Picture of Page","Detailed description");
-    
-  var LoginBtn = page.FindElement("//a[contains(., 'Log in')]");
-  LoginBtn.click();
-  page.Wait(5000);
-  
-  aqObject.CheckProperty(page.FindElement("//h1[.='Sign In']"), "contentText", cmpEqual, "Sign In");
-  Log.Picture(page.Picture(), "Picture of Page","Detailed description");
-  
-  var UName = page.FindElement("#UsernameOrEmail");
-  var PWord = page.FindElement("#Password")
-  UName.value = "OldSailor1960";  
-  PWord.value = "Smartbear1!";
-  page.Wait(5000);
-  Log.Picture(page.Picture(), "Picture of Page","Detailed description");
-  page.FindElement("//button[contains(text(), 'Log in')]").click();
-  page.Wait(5000);
-  Log.Picture(page.Picture(), "Picture of Page","Detailed description");
+  //The beginning of the Simple_Test group
+  //Creates a log folder and makes it the current folder to which messages will be posted. This folder can contain messages of different types as well as subfolders.
+  Log.AppendFolder("Simple_Test", "", pmNormal, Project.Variables.LogAtrribTestTitle);
+  //The beginning of the Edge group
+  //Creates a log folder and makes it the current folder to which messages will be posted. This folder can contain messages of different types as well as subfolders.
+  Log.AppendFolder("Open Close Edge", "", pmNormal, Project.Variables.LogAtrribModTitle);
+  //Launches the specified browser and opens the specified URL in it.
+  Browsers.Item(btEdge).Run("https://bearstore-testsite.smartbear.com/");
+  //Maximizes the specified Window object.
+  Aliases.browser.BrowserWindow.Maximize();
+  //Posts an information message to the test log.
+  Log.Message(Aliases.browser.FileVersionInfo, "");
+  //Checks whether the 'contentText' property of the Aliases.browser.pageShop.sectionContent.panelPhTitle6 object equals 'Welcome to our store.'.
+  aqObject.CheckProperty(Aliases.browser.pageShop.sectionContent.panelPhTitle6, "contentText", cmpEqual, "Welcome to our store.");
+  //Pops the folder that is currently at the top of the folder stack out of the stack. This makes the folder that will become the top of the stack the default folder of the test log.
+  Log.PopLogFolder();
+  //The end of the Edge group
+  //The beginning of the Mod_Login_SampleUser group
+  //Creates a log folder and makes it the current folder to which messages will be posted. This folder can contain messages of different types as well as subfolders.
+  Log.AppendFolder("Mod_Login_SampleUser", "", pmNormal, Project.Variables.LogAtrribModTitle);
+  //Posts an information message to the test log.
+  Log.Message("Mod_Login_SampleUser", "");
+  //Replaces the current indicator text with the specified one.
+  Indicator.PushText("Mod_Login_SampleUser");
+  //Maximizes the specified Window object.
+  Aliases.browser.BrowserWindow.Maximize();
+  //Simulates a user action over the area that contains the recognized text.
+  OCR.Recognize(Aliases.browser.pageShop.header.navUsd).BlockByText("LOG IN").Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageLogin.Wait();
+  //Enters text in the text box.
+  Aliases.browser.pageLogin.sectionContent.textboxUsernameoremail.SetText("SampleUser_001!");
+  //Enters text in the text box.
+  Aliases.browser.pageLogin.sectionContent.passwordboxPassword.SetText("SampleUser_001!");
+  //Clicks the 'buttonLogIn' button.
+  Aliases.browser.pageLogin.sectionContent.buttonLogIn.ClickButton();
+  //Delays the test execution for the specified time period.
+  Delay(3000);
+  //Posts an image to the test log.
+  Log.Picture(Aliases.browser.pageShop.header.panelUsd.Picture(), "Header Bar", "");
+  //Restores the previous indicator text.
+  Indicator.PopText();
+  //Pops the folder that is currently at the top of the folder stack out of the stack. This makes the folder that will become the top of the stack the default folder of the test log.
+  Log.PopLogFolder();
+  //The end of the Mod_Login_SampleUser group
+  //The beginning of the Mod_Logout_SampleUser group
+  //Creates a log folder and makes it the current folder to which messages will be posted. This folder can contain messages of different types as well as subfolders.
+  Log.AppendFolder("Mod_Logout_SampleUser", "", pmNormal, Project.Variables.LogAtrribModTitle);
+  //Posts an information message to the test log.
+  Log.Message("Mod_Logout_SampleUser", "");
+  //Replaces the current indicator text with the specified one.
+  Indicator.PushText("Mod_Logout_SampleUser");
+  //Maximizes the specified Window object.
+  Aliases.browser.BrowserWindow.Maximize();
+  //Clicks the 'textnodeSampleuser001' control.
+  Aliases.browser.pageShop.header.navUsd.linkSampleuser001.textnodeSampleuser001.Click();
+  //Clicks the 'textnodeLogOut' control.
+  Aliases.browser.pageShop.header.navUsd.linkLogOut.textnodeLogOut.Click();
+  //Delays the test execution for the specified time period.
+  Delay(3000);
+  //Posts an image to the test log.
+  Log.Picture(Aliases.browser.pageShop.header.panelUsd.Picture(), "Header Bar", "");
+  //Restores the previous indicator text.
+  Indicator.PopText();
+  //Pops the folder that is currently at the top of the folder stack out of the stack. This makes the folder that will become the top of the stack the default folder of the test log.
+  Log.PopLogFolder();
+  //The end of the Mod_Logout_SampleUser group
+  //The beginning of the Mod_Close_Browser group
+  //Closes the currently running browser
+  //
+  //Creates a log folder and makes it the current folder to which messages will be posted. This folder can contain messages of different types as well as subfolders.
+  Log.AppendFolder("Mod_Close_Browser", "Closes the currently opened browser", pmNormal, Project.Variables.LogAtrribModTitle);
+  //Replaces the current indicator text with the specified one.
+  Indicator.PushText("Mod_Close_Browser");
+  //Closes the browser.
+  Aliases.browser.Close();
+  //Pops the folder that is currently at the top of the folder stack out of the stack. This makes the folder that will become the top of the stack the default folder of the test log.
+  Log.PopLogFolder();
+  //Restores the previous indicator text.
+  Indicator.PopText();
+  //The end of the Mod_Close_Browser group
+  //Pops the folder that is currently at the top of the folder stack out of the stack. This makes the folder that will become the top of the stack the default folder of the test log.
+  Log.PopLogFolder();
+  //The end of the Simple_Test group
 }
