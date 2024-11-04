594c55db0ad60fda74293c930db391b0d151175a
Kevin M Johnson <Kevin.Johnson@Smartbear.Com>
2024-11-04 11:44:30 -0500

    Initial Commit

---
+++ WebTesting/WebTesting_Project/Script/Module_Scripts.js
@@ -0,0 +1,136 @@
+﻿function Script_Mod_Template()
+{
+  //**** Consider duplicating this text as comment inside of the keyword test
+  //**** This can serve as a flowerbox description when converted to a script
+  //**** KMJ 09122022
+  //Replaces the current indicator text with the specified one.
+  Indicator.PushText("Script_Mod_Template");
+  //Creates a log folder and makes it the current folder to which messages will be posted. This folder can contain messages of different types as well as subfolders.
+  Log.AppendFolder("Script_Mod_Template", "All test modules should follow this formatting in order to provide consistency to the look and feel of output logs", pmNormal, Project.Variables.LogAtrribModTitle);
+  //The beginning of the Mod_Template_Main_Operations_Group group
+  //Posts an information message to the test log.
+  Log.Message("Module Template Information Message", "Log entry in the Mod_Template Keyword Test", pmNormal, Project.Variables.LogAtrribInformation);
+  //The end of the Mod_Template_Main_Operations_Group group
+  //Pops the folder that is currently at the top of the folder stack out of the stack. This makes the folder that will become the top of the stack the default folder of the test log.
+  Log.PopLogFolder();
+  //Restores the previous indicator text.
+  Indicator.PopText();
+}
+
+function Test_Generic_Headless()
+{
+// If called from the script is called from a ``Test Execution Plan as a Local Headless Browser, a browser connection is not needed 
+  
+  aqObject.CheckProperty(Aliases.browser.pageShop.sectionContent.panelPhTitle6, "contentText", cmpEqual, "Welcome to our store.");
+  //Posts an image to the test log.
+  Log.Picture(Aliases.browser.pageShop.sectionContent.panelPhTitle6.Picture(), "", "");
+  
+  //Checks whether the 'contentText' property of the Aliases.browser.pageShop.FindElement("//h1[.='Welcome to our store.']") object equals 'Welcome to our store.'.
+  aqObject.CheckProperty(Aliases.browser.pageShop.FindElement("//h1[.='Welcome to our store.']"), "contentText", cmpEqual, "Welcome to our store.");
+  //Clicks the 'linkContactUs' link.
+  Aliases.browser.pageShop.header.navUsd.navContactUs.linkContactUs.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageContactus.Wait();
+  //Checks whether the 'contentText' property of the Aliases.browser.pageContactus.FindElement("//h1[.='Contact Us']") object equals 'Contact Us'.
+  aqObject.CheckProperty(Aliases.browser.pageContactus.FindElement("//h1[.='Contact Us']"), "contentText", cmpEqual, "Contact Us");
+  //Clicks the 'imageSmartstore' control.
+  Aliases.browser.pageContactus.header.link.imageSmartstore.Click();
+  Log.Picture(Aliases.browser.BrowserWindow.Picture(), "", "");
+  //Closes the browser.
+  Aliases.browser.Close();
+  
+}
+
+function Test_Edge_Headless()
+{
+  var server = "localhost";
+  var capabilities = {
+    "browserName": "edge",
+    "screenResolution": "1920x1080"
+  };
+
+  var url = "https://bearstore-testsite.smartbear.com/";
+  Browsers.RemoteItem(server, capabilities).Run(url);
+  
+  aqObject.CheckProperty(Aliases.browser.pageShop.sectionContent.panelPhTitle6, "contentText", cmpEqual, "Welcome to our store.");
+  //Posts an image to the test log.
+  Log.Picture(Aliases.browser.pageShop.sectionContent.panelPhTitle6.Picture(), "", "");
+  
+  //Checks whether the 'contentText' property of the Aliases.browser.pageShop.FindElement("//h1[.='Welcome to our store.']") object equals 'Welcome to our store.'.
+  aqObject.CheckProperty(Aliases.browser.pageShop.FindElement("//h1[.='Welcome to our store.']"), "contentText", cmpEqual, "Welcome to our store.");
+  //Clicks the 'linkContactUs' link.
+  Aliases.browser.pageShop.header.navUsd.navContactUs.linkContactUs.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageContactus.Wait();
+  //Checks whether the 'contentText' property of the Aliases.browser.pageContactus.FindElement("//h1[.='Contact Us']") object equals 'Contact Us'.
+  aqObject.CheckProperty(Aliases.browser.pageContactus.FindElement("//h1[.='Contact Us']"), "contentText", cmpEqual, "Contact Us");
+  //Clicks the 'imageSmartstore' control.
+  Aliases.browser.pageContactus.header.link.imageSmartstore.Click();
+  Log.Picture(Aliases.browser.BrowserWindow.Picture(), "", "");
+  //Closes the browser.
+  Aliases.browser.Close();
+  
+}
+
+function Test_Chrome_Headless()
+{
+  var server = "localhost";
+  var capabilities = {
+    "browserName": "chrome",
+    "screenResolution": "1920x1080"
+  };
+
+  var url = "https://bearstore-testsite.smartbear.com/";
+  Browsers.RemoteItem(server, capabilities).Run(url);
+  
+  aqObject.CheckProperty(Aliases.browser.pageShop.sectionContent.panelPhTitle6, "contentText", cmpEqual, "Welcome to our store.");
+  //Posts an image to the test log.
+  Log.Picture(Aliases.browser.pageShop.sectionContent.panelPhTitle6.Picture(), "", "");
+  
+  //Checks whether the 'contentText' property of the Aliases.browser.pageShop.FindElement("//h1[.='Welcome to our store.']") object equals 'Welcome to our store.'.
+  aqObject.CheckProperty(Aliases.browser.pageShop.FindElement("//h1[.='Welcome to our store.']"), "contentText", cmpEqual, "Welcome to our store.");
+  //Clicks the 'linkContactUs' link.
+  Aliases.browser.pageShop.header.navUsd.navContactUs.linkContactUs.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageContactus.Wait();
+  //Checks whether the 'contentText' property of the Aliases.browser.pageContactus.FindElement("//h1[.='Contact Us']") object equals 'Contact Us'.
+  aqObject.CheckProperty(Aliases.browser.pageContactus.FindElement("//h1[.='Contact Us']"), "contentText", cmpEqual, "Contact Us");
+  //Clicks the 'imageSmartstore' control.
+  Aliases.browser.pageContactus.header.link.imageSmartstore.Click();
+  Log.Picture(Aliases.browser.BrowserWindow.Picture(), "", "");
+  //Closes the browser.
+  Aliases.browser.Close();
+  
+}
+
+function Test_Firefox_Headless()
+{
+  var server = "localhost";
+  var capabilities = {
+    "browserName": "firefox",
+    "screenResolution": "1920x1080"
+  };
+
+  var url = "https://bearstore-testsite.smartbear.com/";
+  Browsers.RemoteItem(server, capabilities).Run(url);
+  
+  aqObject.CheckProperty(Aliases.browser.pageShop.sectionContent.panelPhTitle6, "contentText", cmpEqual, "Welcome to our store.");
+  //Posts an image to the test log.
+  Log.Picture(Aliases.browser.pageShop.sectionContent.panelPhTitle6.Picture(), "", "");
+  
+  //Checks whether the 'contentText' property of the Aliases.browser.pageShop.FindElement("//h1[.='Welcome to our store.']") object equals 'Welcome to our store.'.
+  aqObject.CheckProperty(Aliases.browser.pageShop.FindElement("//h1[.='Welcome to our store.']"), "contentText", cmpEqual, "Welcome to our store.");
+  //Clicks the 'linkContactUs' link.
+  Aliases.browser.pageShop.header.navUsd.navContactUs.linkContactUs.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageContactus.Wait();
+  //Checks whether the 'contentText' property of the Aliases.browser.pageContactus.FindElement("//h1[.='Contact Us']") object equals 'Contact Us'.
+  aqObject.CheckProperty(Aliases.browser.pageContactus.FindElement("//h1[.='Contact Us']"), "contentText", cmpEqual, "Contact Us");
+  //Clicks the 'imageSmartstore' control.
+  Aliases.browser.pageContactus.header.link.imageSmartstore.Click();
+  Log.Picture(Aliases.browser.BrowserWindow.Picture(), "", "");
+  //Closes the browser.
+  Aliases.browser.Close();
+  
+}
+
---
+++ WebTesting/WebTesting_Project/Script/Wait_Samples.js
@@ -0,0 +1,41 @@
+﻿function Sample_Wait_Loop()
+{
+  var Counter;
+  Counter = 0;
+  //**** Sample Demo of Waits - KMJ 
+  //Replaces the current indicator text with the specified one.
+  Indicator.PushText("Sample_Wait_Loop");
+  //Creates a log folder and makes it the current folder to which messages will be posted. This folder can contain messages of different types as well as subfolders.
+  Log.AppendFolder("Sample_Wait_Loop", "**** Sample Demo of Wait Loop - KMJ ", pmNormal, Project.Variables.LogAtrribTestTitle);
+  //Runs a keyword test.
+  KeywordTests.Mod_Open_BearStore_Chrome.Run();
+  //The beginning of the Wait Loop Sample group
+  //Creates a log folder and makes it the current folder to which messages will be posted. This folder can contain messages of different types as well as subfolders.
+  Log.AppendFolder("Sample Wait Loop", "");
+  Counter = 0;
+  while(Aliases.browser.pageShop.header.navUsd.linkLogIn.Exists == false && Counter < 3)
+  {
+    //Posts a warning to the test log.
+    Log.Warning("Log In not found", "");
+    Counter = Counter + 1;
+  }
+  if(Counter == 3)
+  {
+    //Posts an error to the test log.
+    Log.Error("Log In not found and counter exceeded limit", "");
+  }
+  else
+  {
+    //Posts an information message to the test log.
+    Log.Message("Log In found and counter less than limit", "");
+  }
+  //Pops the folder that is currently at the top of the folder stack out of the stack. This makes the folder that will become the top of the stack the default folder of the test log.
+  Log.PopLogFolder();
+  //The end of the Wait Loop Sample group
+  //Runs a keyword test.
+  KeywordTests.Mod_Close_Browser.Run();
+  //Pops the folder that is currently at the top of the folder stack out of the stack. This makes the folder that will become the top of the stack the default folder of the test log.
+  Log.PopLogFolder();
+  //Restores the previous indicator text.
+  Indicator.PopText();
+}
