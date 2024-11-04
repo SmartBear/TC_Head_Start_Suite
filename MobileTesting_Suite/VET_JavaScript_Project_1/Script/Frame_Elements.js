f90c5fc2d1ff47856a928ca3aebbf0bf1ebdceca
Kevin M Johnson <Kevin.Johnson@Smartbear.Com>
2024-11-04 15:23:01 -0500

    Add basic path clone

---
+++ MobileTesting_Suite/VET_JavaScript_Project_1/Script/Frame_Elements.js
@@ -0,0 +1,5 @@
+﻿function Sample_Run_Browser()
+{
+  Browsers.Item("chrome").RunOptions = "--disable-web-security --user-data-dir=\"C:\\Users\\kevin.johnson\\AppData\\Local\\Google\\Chrome\\User Data\\Default\" --disable-site-isolation-trials --test-type";
+  Browsers.Item(btChrome).Run("");
+}
