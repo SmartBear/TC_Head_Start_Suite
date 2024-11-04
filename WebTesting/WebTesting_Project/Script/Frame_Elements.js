c7bb93d71bce9ad1dd398c79ac43c4997379167a
Kevin M Johnson <Kevin.Johnson@Smartbear.Com>
2024-11-04 11:53:17 -0500

    Initial Commit

--- WebTesting/WebTesting_Project/Script/Frame_Elements.js
+++
@@ -1,5 +0,0 @@
-﻿function Sample_Run_Browser()
-{
-  Browsers.Item("chrome").RunOptions = "--disable-web-security --user-data-dir=\"C:\\Users\\kevin.johnson\\AppData\\Local\\Google\\Chrome\\User Data\\Default\" --disable-site-isolation-trials --test-type";
-  Browsers.Item(btChrome).Run("");
-}
